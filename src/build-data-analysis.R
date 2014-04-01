9dd41810c4cce89e59fdabc35bea7a6998e1a751
Mauro Taraborelli <mauro@maurotaraborelli.com>
2014-04-02 00:14:44 +0200

    Modified R script to create PDF.

--- src/build-data-analysis.R
+++ src/build-data-analysis.R
@@ -1,11 +1,9 @@
 # Set working directory
-#setwd("./src/")
+#setwd('./src/')
 
 # Load packages
 require(knitr)
-require(markdown)
 
 # Create .md, .html, and .pdf files
 knit("data-analysis.Rmd")
-#markdownToHTML('data-analysis.md', 'data-analysis.html', options=c("use_xhml"))
-system("pandoc --latex-engine=xelatex --no-highlight --number-sections -V geometry:vmargin=.8in -V geometry:hmargin=.5in -s data-analysis.md -o data-analysis.pdf")
+system("pandoc --latex-engine=xelatex --highlight-style=haddock --number-sections -V geometry:vmargin=.8in -V geometry:hmargin=.5in -s data-analysis.md -o data-analysis.pdf")
