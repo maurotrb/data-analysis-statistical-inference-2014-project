f39c932aeb77ce357b9ea66a259c1dfb8437b5b9
Mauro Taraborelli <mauro@maurotaraborelli.com>
2014-04-01 18:22:49 +0200

    Modified R script to create PDF. Now create directly from markdown.

--- src/build-data-analysis.R
+++ src/build-data-analysis.R
@@ -7,5 +7,5 @@ require(markdown)
 
 # Create .md, .html, and .pdf files
 knit("data-analysis.Rmd")
-markdownToHTML('data-analysis.md', 'data-analysis.html', options=c("use_xhml"))
-system("pandoc -V geometry:vmargin=.75in -V geometry:hmargin=.5in -s data-analysis.html -o data-analysis.pdf")
+#markdownToHTML('data-analysis.md', 'data-analysis.html', options=c("use_xhml"))
+system("pandoc --latex-engine=xelatex --no-highlight --number-sections -V geometry:vmargin=.8in -V geometry:hmargin=.5in -s data-analysis.md -o data-analysis.pdf")
